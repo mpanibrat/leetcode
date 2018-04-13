31406962feeb4bb8b35e54ccdfffbd6e0029c7b5
Maksym Panibratenko <mpanibrat@gmail.com>
2018-04-12 22:16:04 -0700

    783. Minimum Distance Between BST Nodes

---
+++ src/main/kotlin/com/panibrat/Minimum_Distance_Between_BST_Nodes_783/TreeNode.kt
@@ -0,0 +1,9 @@
+package com.panibrat.Minimum_Distance_Between_BST_Nodes_783
+
+/**
+ * Definition for a binary tree node.
+ */
+data class TreeNode(var `val`: Int = 0) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
