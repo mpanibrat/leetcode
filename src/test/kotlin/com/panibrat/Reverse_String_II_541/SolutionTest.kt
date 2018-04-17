e20628c2acb3bc2cbfe227288e9d6c1122707444
Maksym Panibratenko <mpanibrat@gmail.com>
2018-04-17 15:24:52 -0700

    541. Reverse String II

---
+++ src/test/kotlin/com/panibrat/Reverse_String_II_541/SolutionTest.kt
@@ -0,0 +1,17 @@
+package com.panibrat.Reverse_String_II_541
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class SolutionTest {
+
+    val sol = Solution()
+
+    @Test
+    fun reverseStr() {
+        val s = "abcdefg"
+        val k = 2
+        val out = "bacdfeg"
+        assertThat(sol.reverseStr(s, k)).isEqualTo(out)
+    }
+}
