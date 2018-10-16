d7e82258a7d32406caf881bfbb3d1682ef90b026
Maksym Panibratenko <mpanibrat@gmail.com>
2018-10-15 22:37:05 -0700

    226. Invert Binary Tree

---
+++ src/main/java/com/panibrat/tree/_226/Solution.java
@@ -0,0 +1,48 @@
+package com.panibrat.tree._226;
+
+/*
+226. Invert Binary Tree
+
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+ */
+
+import com.panibrat.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null) return root;
+    TreeNode tmp = root.left;
+    root.left = root.right;
+    root.right = tmp;
+    invertTree(root.left);
+    invertTree(root.right);
+    return root;
+  }
+}
