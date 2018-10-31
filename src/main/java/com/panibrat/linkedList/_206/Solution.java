4e9636f315e1f90aba8dc2afadc36eec5bc2c183
Maksym Panibratenko <mpanibrat@gmail.com>
2018-10-30 22:24:53 -0700

    206. Reverse Linked List

---
+++ src/main/java/com/panibrat/linkedList/_206/Solution.java
@@ -0,0 +1,49 @@
+package com.panibrat.linkedList._206;
+
+/*
+206. Reverse Linked List
+
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+Follow up:
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+
+ */
+
+import com.panibrat.linkedList.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public ListNode reverseListRec(ListNode head) {
+    if (head == null || head.next == null) return head;
+    ListNode node = reverseListRec(head.next);
+    head.next.next = head;
+    head.next = null;
+    return node;
+  }
+
+  public ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    ListNode curr = head;
+    while (curr != null) {
+      ListNode next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    return prev;
+  }
+}
