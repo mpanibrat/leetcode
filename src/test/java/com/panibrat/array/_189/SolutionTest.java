cea05cca6995b409078ad838530ed3260ae9f5c9
Maksym Panibratenko <mpanibrat@gmail.com>
2019-04-27 23:47:42 -0500

    189. Rotate Array

---
+++ src/test/java/com/panibrat/array/_189/SolutionTest.java
@@ -0,0 +1,48 @@
+package com.panibrat.array._189;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+/*
+189. Rotate Array
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+ */
+public class SolutionTest {
+
+  @Test
+  public void rotate_1() {
+    int[] nums = {1, 2, 3, 4, 5, 6, 7};
+    new Solution().rotate(nums, 3);
+    assertArrayEquals(new int[]{5, 6, 7, 1, 2, 3, 4}, nums);
+  }
+
+  @Test
+  public void rotate_2() {
+    int[] nums = {-1, -100, 3, 99};
+    new Solution().rotate(nums, 2);
+    assertArrayEquals(new int[]{3, 99, -1, -100}, nums);
+  }
+}
