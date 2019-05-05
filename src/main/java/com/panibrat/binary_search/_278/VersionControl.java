105e592df4f74ee4accbfb4b9f38b8d226d55c51
Maksym Panibratenko <mpanibrat@gmail.com>
2019-05-04 21:31:07 -0500

    278. First Bad Version

---
+++ src/main/java/com/panibrat/binary_search/_278/VersionControl.java
@@ -0,0 +1,15 @@
+package com.panibrat.binary_search._278;
+
+public class VersionControl {
+
+  private final int bad;
+
+  public VersionControl(int bad) {
+    this.bad = bad;
+  }
+
+  boolean isBadVersion(int version) {
+    return bad == version;
+  }
+
+}
