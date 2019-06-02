0c2350b7178e9272e1065e6ecaa4d060ed84169a
Maksym Panibratenko <mpanibrat@gmail.com>
2019-06-01 19:47:56 -0500

    461. Hamming Distance

---
+++ src/test/java/com/panibrat/bit_manipulation/_461/SolutionTest.java
@@ -0,0 +1,36 @@
+package com.panibrat.bit_manipulation._461;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/*
+461. Hamming Distance
+
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+ */
+public class SolutionTest {
+
+  @Test
+  public void hammingDistance() {
+    assertEquals(2, new Solution().hammingDistance(1, 4));
+  }
+}
